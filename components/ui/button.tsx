af9bae6e9434d6ad50c408fe70875eb9609970cf
basuoluomiu <sebas.pelayo@gmail.com>
2026-02-12 00:27:39 +0800

    Fix: Resolve Framer Motion type conflict in Button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -33,7 +33,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
           },
           className
         )}
-        {...props}
+        {...(props as any)}
       >
         {children}
       </motion.button>
